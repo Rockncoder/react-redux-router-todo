c25b4f9a1d7df4b19a99c87495846765e1dd434e
rockncoder <rockncoder@gmail.com>
2017-12-11 05:13:47 -0800

    Initial commit

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import Container from './Container';
+import reducer from './reducer';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: 'all'
+    }
+  }
+
+  setVisibilityFilter = (filter) => {
+    this.setState({filter});
+  };
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-2"></div>
+            <div className="column is-8">
+              <Container />
+            </div>
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default Todo
---
+++ src/components/Todo/TodoList.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react';
+import TodoItem from './TodoItem';
+
+export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputText: '',
+    };
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const trimmed = this.state.inputText.trim();
+    if (trimmed.length > 0) {
+      this.props.addTodo(trimmed);
+      this.setState({
+        inputText: ''
+      });
+    }
+  };
+
+  handleOnChange = event => {
+    const key = event.target.value;
+    this.setState({
+      inputText: key
+    });
+  };
+
+  remove(id) {
+
+  }
+
+  toggle(id) {
+    this.props.toggleTodo(id);
+  }
+
+  renderTodoList() {
+    let filteredList = [];
+
+    switch (this.props.displayType) {
+      case 'active':
+        filteredList = this.props.todos.filter(item => !item.completed);
+        break;
+      case 'completed':
+        filteredList = this.props.todos.filter(item => item.completed);
+        break;
+      default:
+        filteredList = [...this.props.todos];
+    }
+
+    return filteredList.map((item, index) => (
+      <TodoItem
+        toggle={() => this.toggle(index)}
+        remove={() => this.remove(index)}
+        item={item}
+        key={index}
+      />
+    ));
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="section">
+          <div>
+            <form onSubmit={this.onSubmit} className="columns">
+              <div className="column is-four-fifths">
+                <input
+                  type="text"
+                  className="input column"
+                  placeholder="Add a Todo"
+                  value={this.state.inputText}
+                  onChange={this.handleOnChange}
+                  maxLength={20}
+                />
+              </div>
+              <div className="column has-text-left">
+                <span className="icon has-text-primary fa fa-2x" onClick={this.onSubmit}>
+                  <i className="fa fa-plus"/>
+                </span>
+              </div>
+            </form>
+          </div>
+          <div>
+            {this.renderTodoList()}
+          </div>
+        </div>
+
+        <footer className="footer">
+          <div className="content has-text-centered">
+            <button className="button" onClick={() => this.props.setVisibilityFilter('all')}>
+              All
+            </button>
+            <button className="button" onClick={() => this.props.setVisibilityFilter('completed')}>
+              Completed
+            </button>
+            <button className="button" onClick={() => this.props.setVisibilityFilter('active')}>
+              Active
+            </button>
+          </div>
+        </footer>
+      </section>
+    );
+  }
+}
